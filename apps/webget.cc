dfaa97ea2326d99aff71e7f8bbcffc1223021514
sgt <sgt@email.com>
2023-12-07 23:40:07 +0800

    the code of webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,8 +9,19 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  TCPSocket socket;
+  socket.connect( Address( host, "http" ) );
+  socket.write( "GET " + path + " HTTP/1.1\r\n" );
+  socket.write( "HOST: " + host + "\r\n" );
+  socket.write( "Connection: close\r\n" );
+  socket.write( "\r\n" );
+  socket.shutdown( SHUT_WR );
+  string buf;
+  while ( !socket.eof() ) {
+    socket.read( buf );
+    cout << buf;
+  }
+  socket.close();
 }
 
 int main( int argc, char* argv[] )

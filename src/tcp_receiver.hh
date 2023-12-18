84e5f6969f3ed4fc3325fff6f793de2457d96af0
sgt <sgt@email.com>
2023-12-18 15:49:06 +0800

    finish the code of tcp_receiver

--- src/tcp_receiver.hh
+++ src/tcp_receiver.hh
@@ -6,6 +6,11 @@
 
 class TCPReceiver
 {
+protected:
+  Wrap32 ISN;       //flag ISN
+  Wrap32 fin;       //flag fin
+  bool is_set_ISN;   //is set ISN
+  bool is_end;      //is end?
 public:
   /*
    * The TCPReceiver receives TCPSenderMessages, inserting their payload into the Reassembler
@@ -15,4 +20,5 @@ public:
 
   /* The TCPReceiver sends TCPReceiverMessages back to the TCPSender. */
   TCPReceiverMessage send( const Writer& inbound_stream ) const;
+  TCPReceiver():ISN(0),fin(0),is_set_ISN(0),is_end(false){}
 };
